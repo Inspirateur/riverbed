49e75bc1ca83a9bb29293d83021f04a9961f3c47
kmorrisongr <mckinleybagel@pm.me>
2026-01-16 21:19:54 -0500

    reorg block_entities based on trait impl rules

--- server/src/world/block_entities.rs
+++ server/src/world/block_entities.rs
@@ -1,36 +1,7 @@
 use bevy::prelude::*;
-use hashbrown::HashMap;
-use shared::world::{block_entities::BlockEntities, pos::{BlockPos, ColPos}};
+use shared::world::block_entities::BlockEntities;
 
-
-impl BlockEntitiesTrait for BlockEntities {
-    fn unload_col(&mut self, col_pos: &ColPos) -> Vec<Entity> {
-        let Some(entities) = self.0.remove(col_pos) else {
-            return Vec::new()
-        };
-        entities.into_values().into_iter().collect()
-    }
-
-    fn get(&self, block_pos: &BlockPos) -> Option<Entity> {
-        let (col_pos, pos): (ColPos, (usize, i32, usize)) = (*block_pos).into();
-        let col_ents = self.0.get(&col_pos)?;
-        col_ents.get(&pos).copied()
-    }
-
-    fn remove(&mut self, block_pos: &BlockPos) {
-        let (col_pos, pos): (ColPos, (usize, i32, usize)) = (*block_pos).into();
-        let Some(entities) = self.0.get_mut(&col_pos) else {
-            return;
-        };
-        entities.remove(&pos);
-    }
-
-    fn add(&mut self, block_pos: &BlockPos, entity: Entity) {
-        let (col_pos, pos) = (*block_pos).into();
-        self.0.entry(col_pos).or_default().insert(pos, entity);
-    }
-}
-
+use crate::world::ColUnloadEvent;
 
 
 pub fn unload_block_entities(
--- shared/src/world/block_entities.rs
+++ shared/src/world/block_entities.rs
@@ -4,11 +4,32 @@ use hashbrown::HashMap;
 use crate::world::pos::{BlockPos, ColPos};
 
 #[derive(Default, Clone, Resource)]
-pub struct BlockEntities(pub HashMap<ColPos, HashMap<(usize, i32, usize), Entity>>);
+pub struct BlockEntities(HashMap<ColPos, HashMap<(usize, i32, usize), Entity>>);
 
-pub trait BlockEntitiesTrait {
-    fn unload_col(&mut self, col_pos: &ColPos) -> Vec<Entity>;
-    fn get(&self, block_pos: &BlockPos) -> Option<Entity>;
-    fn remove(&mut self, block_pos: &BlockPos);
-    fn add(&mut self, block_pos: &BlockPos, entity: Entity);
+impl BlockEntities {
+    pub fn unload_col(&mut self, col_pos: &ColPos) -> Vec<Entity> {
+        let Some(entities) = self.0.remove(col_pos) else {
+            return Vec::new()
+        };
+        entities.into_values().into_iter().collect()
+    }
+
+    pub fn get(&self, block_pos: &BlockPos) -> Option<Entity> {
+        let (col_pos, pos): (ColPos, (usize, i32, usize)) = (*block_pos).into();
+        let col_ents = self.0.get(&col_pos)?;
+        col_ents.get(&pos).copied()
+    }
+
+    pub fn remove(&mut self, block_pos: &BlockPos) {
+        let (col_pos, pos): (ColPos, (usize, i32, usize)) = (*block_pos).into();
+        let Some(entities) = self.0.get_mut(&col_pos) else {
+            return;
+        };
+        entities.remove(&pos);
+    }
+
+    pub fn add(&mut self, block_pos: &BlockPos, entity: Entity) {
+        let (col_pos, pos) = (*block_pos).into();
+        self.0.entry(col_pos).or_default().insert(pos, entity);
+    }
 }
