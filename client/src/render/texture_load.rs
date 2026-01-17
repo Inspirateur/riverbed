3422c961cab86a5a72e3b0d40fe67e390d36e890
kmorrisongr <mckinleybagel@pm.me>
2026-01-17 13:36:27 -0500

    EventReader -> MessageReader

--- client/src/render/texture_load.rs
+++ client/src/render/texture_load.rs
@@ -38,7 +38,7 @@ fn load_block_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn check_block_textures(
     mut next_state: ResMut<NextState<BlockTexState>>,
     texture_folder: ResMut<BlockTextureFolder>,
-    mut events: EventReader<AssetEvent<LoadedFolder>>,
+    mut events: MessageReader<AssetEvent<LoadedFolder>>,
 ) {
     // Advance the `AppState` once all sprite handles have been loaded by the `AssetServer`
     for event in events.read() {
@@ -81,7 +81,7 @@ fn load_item_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn check_item_textures(
     mut next_state: ResMut<NextState<ItemTexState>>,
     texture_folder: ResMut<ItemTextureFolder>,
-    mut events: EventReader<AssetEvent<LoadedFolder>>,
+    mut events: MessageReader<AssetEvent<LoadedFolder>>,
 ) {
     // Advance the `AppState` once all sprite handles have been loaded by the `AssetServer`
     for event in events.read() {
