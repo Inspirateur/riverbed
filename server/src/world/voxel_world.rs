0d489ec839d10141d0e9146c8758a84f6dabdd46
kmorrisongr <mckinleybagel@pm.me>
2026-01-16 21:09:07 -0500

    reorg dependencies

--- server/src/world/voxel_world.rs
+++ server/src/world/voxel_world.rs
@@ -2,7 +2,7 @@ use bevy::prelude::{Resource, Vec3};
 use crossbeam::channel::Sender;
 use crossbeam_skiplist::{map::Entry, SkipMap};
 use parking_lot::RwLock;
-use shared::{block::Block, world::{chunk::Chunk, pos::{BlockPos, ChunkPos, ColPos, ColedPos, chunked}}};
+use shared::{block::{Block, Face}, world::{CHUNK_S1, CHUNKP_S1, MAX_HEIGHT, Y_CHUNKS, chunk::Chunk, pos::{BlockPos, BlockPos2d, ChunkPos, ChunkedPos, ColPos, ColedPos, chunked, pos2d::chunks_in_col}, realm::Realm}};
 use std::sync::Arc;
 
 pub struct BlockRayCastHit {
